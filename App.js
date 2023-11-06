b48e6b27eeb022e7271f84be2da9db00c0802f63
damcode1990 <damcode1990@gmail.com>
2023-11-06 17:54:27 +0530

    change app.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+// const path = require('path');
 const app = express();
 const errorMiddleware = require("./errors/error.js")
 
@@ -12,16 +12,21 @@ dotenv.config({ path: "config/config.env" })
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 const userRoutes = require('./routes/userroutes.js');
 app.use('/api/v1', userRoutes);
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'html', 'index.html'));
+//   });
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'html', 'index.html'));
-  });
+  res.send('<h1>Working Fine</h1>')
+})
 
 app.use(errorMiddleware);
 
--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// const app = express();
-// const connectDB = require('./db.js');
-// const errorMiddleware = require('./errors/error.js');
-
-// // Body parsers
-// app.use(express.json({ extended: true }));
-
-// // Connect to the database
-// connectDB()
-//   .then(() => {
-//     // Serve static files
-//     app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-//     app.get('/', (req, res) => {
-//       res.sendFile(path.join(__dirname, 'html', 'index.html'));
-//     });
-
-//     // Include your routes
-//     const userRoutes = require('./routes/userroutes.js');
-//     app.use('/api/v1', userRoutes);
-
-//     // Error handling middleware (should be placed after your routes)
-//     app.use(errorMiddleware);
-
-//     const PORT = process.env.PORT || 8000;
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error('Database connection error:', err);
-//   });
 const app = require('./App')
 const dotenv = require('dotenv')
 const connectDatabase = require('./db')
