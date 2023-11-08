42d601ebc79a06ebbd078bb523539fca24e02f4e
SahilPethani <sahilpethani50@gmail.com>
2023-11-08 17:41:15 +0530

    testing add employee 4

--- backend/App.js
+++ backend/App.js
@@ -6,26 +6,27 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require("path");
 
-const allowedOrigins = ['https://hrms-api-six.vercel.app'];
+// const allowedOrigins = ['https://hrms-api-six.vercel.app'];
 
-// Use the cors middleware to allow requests from specific origins
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,POST,PUT,DELETE', // Specify the allowed HTTP methods
-    allowedHeaders: 'Content-Type,Authorization', // Specify the allowed headers
-  })
-);
+// // Use the cors middleware to allow requests from specific origins
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     methods: 'GET,POST,PUT,DELETE', // Specify the allowed HTTP methods
+//     allowedHeaders: 'Content-Type,Authorization', // Specify the allowed headers
+//   })
+// );
 
 const errorMiddleware = require("./errors/error");
 dotenv.config({ path: "backend/config/config.env" });
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
